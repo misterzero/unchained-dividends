bc17d84db06844cb6d086cc4b53f71254b14fe11
crane <kcrane@ippon.fr>
2017-07-26 09:59:35 -0400

    updated HelloWorldService test to deploy chaincode before tests start

--- src/test/java/com/ippon/unchained/service/impl/HelloWorldServiceImplIntTest.java
+++ src/test/java/com/ippon/unchained/service/impl/HelloWorldServiceImplIntTest.java
@@ -2,17 +2,23 @@ package com.ippon.unchained.service.impl;
 
 import com.ippon.unchained.UnchainedApp;
 import com.ippon.unchained.service.HelloWorldService;
+import com.ippon.unchained.service.MailService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.concurrent.ExecutionException;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doNothing;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = UnchainedApp.class)
@@ -20,6 +26,11 @@ public class HelloWorldServiceImplIntTest {
 
     @Autowired
     HelloWorldService helloWorldService;
+
+    @Before
+    public void setup() {
+        helloWorldService.deployHelloWorld();
+    }
 
     @Test
     public void testGetClientVersion() throws IOException {
@@ -43,7 +54,6 @@ public class HelloWorldServiceImplIntTest {
 
     @Test
     public void testDeployHelloWorld() {
-        helloWorldService.deployHelloWorld();
         assertThat(helloWorldService.getContractAddress().trim().length() > 0);
     }
 
