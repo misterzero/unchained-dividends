39259f6f5250d4895908aa23bb67bd0cea9690a5
Andrea ESPOSITO <aesposito@ippon.fr>
2017-07-27 09:42:31 -0400

    dividends API without real functions

---
+++ src/main/java/com/ippon/unchained/service/DividendsService.java
@@ -0,0 +1,13 @@
+package com.ippon.unchained.service;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.concurrent.ExecutionException;
+
+public interface DividendsService extends Web3JService{
+
+    BigDecimal getBalanceDividends() throws ExecutionException, InterruptedException, IOException;
+
+    void deployDividends();
+
+}
